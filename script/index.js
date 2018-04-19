dc14dbbbf397a2f32132206dd416074f3d49bbdd
AndréSDS <andre_smiths@outlook.com>
2018-04-19 03:28:33 -0300

    Acertando detalhes do css

--- script/index.js
+++ script/index.js
@@ -1,12 +1,4 @@
 window.onload = function initiPage() {//aguardando página ser carregada
-//função que muda o nome
-  let mudar = window.setInterval(troca, 1000);
-  function troca() {
-    document.querySelector('.banner span').innerHTML = 'André!';
-  }
-  window.setTimeout(function() {
-    clearInterval(mudar);
-}, 2000);
 
   //funcção pra animar dropdown-icon
     let span = document.querySelector('#dropdown-icon span');
@@ -20,5 +12,16 @@ window.onload = function initiPage() {//aguardando página ser carregada
         document.getElementById('menu').style.height = 0;
       }
     });
+
+  //função pra fazer a barra de navegação sumir ao rolar pra baixo
+  window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            document.getElementById("navbar").style.height = 0;
+        } else {
+            document.getElementById("navbar").style.height = "";
+        }
+      }
 
 }
