8e4669e45bf293f17fc9da94b7dfd2548e7fb0b1
AndréSDS <andre_smiths@outlook.com>
2018-04-19 00:31:58 -0300

    Tá dando sono

--- script/index.js
+++ script/index.js
@@ -1,4 +1,13 @@
 window.onload = function initiPage() {//aguardando página ser carregada
+//função que muda o nome
+  let mudar = window.setInterval(troca, 1000);
+  function troca() {
+    document.querySelector('.banner span').innerHTML = 'André!';
+  }
+  window.setTimeout(function() {
+    clearInterval(mudar);
+}, 2000);
+
   //funcção pra animar dropdown-icon
     let span = document.querySelector('#dropdown-icon span');
     document.getElementById('dropdown-icon').addEventListener('click', function () {
