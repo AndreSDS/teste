6c86a0f4e3c7cc8f317bc389d2ddd17810b38b42
AndréSDS <andre_smiths@outlook.com>
2018-04-20 10:00:40 -0300

    atualizando as animações dorpdown

--- script/index.js
+++ script/index.js
@@ -1,31 +1,52 @@
 window.onload = function initiPage() {//aguardando página ser carregada
 
-  //funcção pra animar dropdown-icon
-    let span = document.querySelector('#dropdown-icon span');
-    document.getElementById('dropdown-icon').addEventListener('click', function () {
-      if (span.className != 'ativo') {
-        span.className = 'ativo';
-        //linha pra exibir o menu dropdown
-        document.getElementById('menu').style.height = 60+'vh';
-      }else {
-        span.className = '';
-        document.getElementById('menu').style.height = 0;
+  //colocando elementos em variaveis
+    const botao = document.getElementById('dropdown-icon');
+    const span = document.querySelector('#dropdown-icon span');
+    const menu = document.getElementById('menu');
+    const navbar = document.getElementById('navbar');
+
+    //animando botão dropdown e menu dropdown
+    botao.addEventListener('click', function () {
+      if (span.className != 'humburger-icon ativo') {
+        span.classList.add('ativo');
+        menu.classList.add('dropdown');
+      }else{
+        span.classList.remove('ativo');
+        menu.classList.remove('dropdown');
       }
-    });
+  });
+
+  //fechar dropdown ao clicar em uma opção
+  let nav_li = document.querySelectorAll('#menu ul li');
+  for (var i = 0; i < nav_li.length; i++) {
+   nav_li[i].addEventListener('click', function () {
+     if (menu.className == 'dropdown') {
+       menu.classList.remove('dropdown');
+       span.classList.remove('ativo');
+     }else{
+       menu.classList.add('sumiu');
+     }
+   })
+  }
+
+  //rolagem suave ao clicar em um li da nav menu
+
 
   //descobrindo se está rolando para baixo ou para cima -https://eduardopereira.pt/2010/05/detectar-direccao-de-scroll-com-javascript-jquery/
-  let subiu = 0;
   let desceu = 0;
-
-  subiu = window.scrollY;
+  let subiu = window.scrollY;
   window.onscroll = function () {
     desceu = window.scrollY;
-      if (subiu > desceu) {
-        document.getElementById('navbar').classList.remove('sumiu');
+      if (subiu < desceu && subiu >= 70) {
+        //se rolar pra baixo a navbar some
+        navbar.classList.add('sumiu');
         subiu = window.scrollY;
       }else {
-        document.getElementById('navbar').classList.add('sumiu');
+        //se rolar pra cima navbar aparece
+        navbar.classList.remove('sumiu');
         subiu = window.scrollY;
       }
   }
-}
+
+}//Função carregamento página
